e0070fd5970c577100018e0257cf3e796cdee757
Harsh Singhaniya <hsinghaniya30@gmail.com>
2026-06-21 22:19:15 +0530

    refactor: Simplify exercise controller and routes by removing unnecessary function wrapper

--- src/modules/exercises/controller.ts
+++ src/modules/exercises/controller.ts
@@ -1,36 +1,28 @@
-import type { Request, Response } from "express";
+import type { RequestHandler } from "express";
 
 import { NotFoundError, ValidationError } from "../../shared/errors.js";
 import { getExerciseById, getExercises } from "./queries.js";
 import { isExerciseCategory } from "./validators.js";
 
-export function createExercisesController() {
-  {
-    async function getAll(req: Request, res: Response) {
-      const rawCategory = req.query.category;
-      const category = isExerciseCategory(rawCategory)
-        ? rawCategory
-        : undefined;
-      if (rawCategory && !category)
-        throw new ValidationError("Invalid category.", {
-          category: `${rawCategory} is not a valid exercise category.`,
-        });
+export const getAll: RequestHandler = async (req, res) => {
+  const rawCategory = req.query.category;
+  const category = isExerciseCategory(rawCategory) ? rawCategory : undefined;
+  if (rawCategory && !category)
+    throw new ValidationError("Invalid category.", {
+      category: `${rawCategory} is not a valid exercise category.`,
+    });
 
-      const exercises = await getExercises({ category });
-      res.json(exercises);
-    }
+  const exercises = await getExercises({ category });
+  res.json(exercises);
+};
 
-    async function getById(req: Request<{ id: string }>, res: Response) {
-      const id = Number(req.params.id);
-      if (isNaN(id))
-        throw new ValidationError("Invalid exercise id.", {
-          id: "Exercise id must be a number.",
-        });
-      const exercise = await getExerciseById(id);
-      if (!exercise) throw new NotFoundError("Exercise not found.");
-      res.json(exercise);
-    }
-
-    return { getAll, getById };
-  }
-}
+export const getById: RequestHandler = async (req, res) => {
+  const id = Number(req.params.id);
+  if (isNaN(id))
+    throw new ValidationError("Invalid exercise id.", {
+      id: "Exercise id must be a number.",
+    });
+  const exercise = await getExerciseById(id);
+  if (!exercise) throw new NotFoundError("Exercise not found.");
+  res.json(exercise);
+};
