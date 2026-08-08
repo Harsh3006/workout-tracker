18d99143f6714813c57caef16b24824f74462ed5
Harsh Singhaniya <hsinghaniya30@gmail.com>
2026-08-09 00:22:13 +0530

    fix: update ESLint configuration for improved TypeScript and React support

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,23 +1,26 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    ignores: ["dist", "node_modules"],
+    ignores: ["**/dist", "**/coverage", "**/node_modules"],
   },
-  tseslint.configs.recommended,
   js.configs.recommended,
+  tseslint.configs.recommended,
+
+  // Shared rules
   {
-    languageOptions: {
-      globals: { ...globals.node },
-    },
+    files: ["**/*.{ts,tsx}"],
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
     rules: {
+      "no-undef": "off",
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
@@ -29,6 +32,23 @@ export default defineConfig([
       ],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+    },
+  },
+
+  // API (Node)
+  {
+    files: ["apps/api/**/*.{ts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  // Web (React)
+  {
+    files: ["apps/web/**/*.{ts,tsx}"],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    languageOptions: {
+      globals: globals.browser,
     },
   },
 ]);
